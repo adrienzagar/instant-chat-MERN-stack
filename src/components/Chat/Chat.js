826ada61b48d31e4fdcce0a5ebfde5adccdb2388
adrien zgr <adrien.zagar@gmail.com>
2020-09-11 12:04:57 +0200

    adding clickable icons

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.scss'
+
+
+const Chat = () => {
+    return (
+        <div className='chat'>
+            I am chat
+        </div>
+    )
+}
+export  default Chat
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './styles.scss';
+import { Avatar, IconButton } from '@material-ui/core'
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header">
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
+
