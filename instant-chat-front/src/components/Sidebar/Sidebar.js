9f61aefbb54c3c4f1f7de5ff4b242703b8c24025
adrien zgr <adrien.zagar@gmail.com>
2020-09-11 17:28:43 +0200

    changing directories

---
+++ instant-chat-front/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './styles.scss';
+import { Avatar, IconButton } from '@material-ui/core'
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchOutlined from '@material-ui/icons/SearchOutlined'
+import SidebarChat from './SidebarChat';
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header">
+                <Avatar src="https://avatars2.githubusercontent.com/u/59897360?s=460&u=ef01dbf9a856c3d927d5f076691ca078bb9cee33&v=4" />
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search of start new chat" type="text"/>
+                </div>
+            </div>
+
+            <div className="sidebar__chats">
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
+
