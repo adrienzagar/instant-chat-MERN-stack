3a9dfe252ab47a5bdc31fbd11325b9cd47082c18
adrien zgr <adrien.zagar@gmail.com>
2020-09-11 12:30:28 +0200

    adding avatar and searchBar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,11 +4,13 @@ import { Avatar, IconButton } from '@material-ui/core'
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchOutlined from '@material-ui/icons/SearchOutlined'
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
             <div className="sidebar__header">
+                <Avatar src="https://avatars2.githubusercontent.com/u/59897360?s=460&u=ef01dbf9a856c3d927d5f076691ca078bb9cee33&v=4" />
                 <div className="sidebar__headerRight">
                     <IconButton>
                         <DonutLargeIcon />
@@ -19,6 +21,13 @@ const Sidebar = () => {
                     <IconButton>
                         <MoreVertIcon />
                     </IconButton>
+                </div>
+            </div>
+
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search of start new chat" type="text"/>
                 </div>
             </div>
         </div>
